0af569f436ee0b448a64c9d7c8b62028599c4ccc
Ali Haugh <alihaugh@Alis-MacBook-Pro.local>
2017-01-25 16:30:44 -0800

    Minimize that code.

--- LifeCounter/app/src/main/java/edu/uw/alihaugh/lifecounter/MainActivity.java
+++ LifeCounter/app/src/main/java/edu/uw/alihaugh/lifecounter/MainActivity.java
@@ -9,8 +9,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final int LIFE_JUMP = 5;
-    private final int INITIAL_LIVES = 20;
     Button mButton1, mButton2, mButton3, mButton4, aButton1, aButton2, aButton3, aButton4,
             m5Button1, m5Button2, m5Button3, m5Button4, a5Button1, a5Button2, a5Button3, a5Button4;
 
@@ -29,240 +27,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // player1 buttons
-        mButton1 = (Button)findViewById(minusButtons[0]);
-        mButton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView lifeCount = (TextView)findViewById(R.id.lives1);
-                int count = Integer.parseInt(lifeCount.getText().toString());
-                count = count - 1;
-                if (count <= 0) {
-                    lifeCount.setText("0");
-                    TextView loser = (TextView)findViewById(R.id.loser);
-                    loser.setText("PLAYER 1 LOSES!!");
-                    loser.setVisibility(View.VISIBLE);
-                } else {
-                    lifeCount.setText(Integer.toString(count));
-                }
-            }
-        });
+        for(int i = 1; i < 5; i++) {
+            mButtonObj[i - 1] = (Button)findViewById(minusButtons[i - 1]);
+            mButtonObj[i - 1].setOnClickListener(new myListener(-1, i));
 
-        aButton1 = (Button)findViewById(plusButtons[0]);
-        aButton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView lifeCount = (TextView)findViewById(R.id.lives1);
-                int count = Integer.parseInt(lifeCount.getText().toString());
-                count = count + 1;
+            aButtonObj[i - 1] = (Button)findViewById(plusButtons[i - 1]);
+            aButtonObj[i - 1].setOnClickListener(new myListener(1, i));
+
+            m5ButtonObj[i - 1] = (Button)findViewById(minus5Buttons[i - 1]);
+            m5ButtonObj[i - 1].setOnClickListener(new myListener(-5, i));
+
+            a5ButtonObj[i - 1] = (Button)findViewById(plus5Buttons[i - 1]);
+            a5ButtonObj[i - 1].setOnClickListener(new myListener(5, i));
+        }
+    }
+
+    public class myListener implements View.OnClickListener {
+        public int change, playerNum;
+
+        public myListener(int lifeChange, int player) {
+            change = lifeChange;
+            playerNum = player;
+        }
+
+        @Override
+        public void onClick(View v) {
+            TextView lifeCount = (TextView)findViewById(lifeCounts[playerNum - 1]);
+            int count = Integer.parseInt(lifeCount.getText().toString());
+            count = count + change;
+            if (count <= 0) {
+                lifeCount.setText("0");
+                TextView loser = (TextView)findViewById(R.id.loser);
+                loser.setText("PLAYER " + playerNum + " LOSES!!");
+                loser.setVisibility(View.VISIBLE);
+            } else {
                 lifeCount.setText(Integer.toString(count));
             }
-        });
+        }
 
-        m5Button1 = (Button)findViewById(minus5Buttons[0]);
-        m5Button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView lifeCount = (TextView)findViewById(R.id.lives1);
-                int count = Integer.parseInt(lifeCount.getText().toString());
-                count = count - 5;
-                if (count <= 0) {
-                    lifeCount.setText("0");
-                    TextView loser = (TextView)findViewById(R.id.loser);
-                    loser.setText("PLAYER 1 LOSES!!");
-                    loser.setVisibility(View.VISIBLE);
-                } else {
-                    lifeCount.setText(Integer.toString(count));
-                }
-            }
-        });
-
-        a5Button1 = (Button)findViewById(plus5Buttons[0]);
-        a5Button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView lifeCount = (TextView)findViewById(R.id.lives1);
-                int count = Integer.parseInt(lifeCount.getText().toString());
-                count = count + 5;
-                lifeCount.setText(Integer.toString(count));
-            }
-        });
-
-        // player2 buttons
-        mButton2 = (Button)findViewById(minusButtons[1]);
-        mButton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView lifeCount = (TextView)findViewById(R.id.lives2);
-                int count = Integer.parseInt(lifeCount.getText().toString());
-                count = count - 1;
-                if (count <= 0) {
-                    lifeCount.setText("0");
-                    TextView loser = (TextView)findViewById(R.id.loser);
-                    loser.setText("PLAYER 2 LOSES!!");
-                    loser.setVisibility(View.VISIBLE);
-                } else {
-                    lifeCount.setText(Integer.toString(count));
-                }
-            }
-        });
-
-        aButton2 = (Button)findViewById(plusButtons[1]);
-        aButton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView lifeCount = (TextView)findViewById(R.id.lives2);
-                int count = Integer.parseInt(lifeCount.getText().toString());
-                count = count + 1;
-                lifeCount.setText(Integer.toString(count));
-            }
-        });
-
-        m5Button2 = (Button)findViewById(minus5Buttons[1]);
-        m5Button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView lifeCount = (TextView)findViewById(R.id.lives2);
-                int count = Integer.parseInt(lifeCount.getText().toString());
-                count = count - 5;
-                if (count <= 0) {
-                    lifeCount.setText("0");
-                    TextView loser = (TextView)findViewById(R.id.loser);
-                    loser.setText("PLAYER 2 LOSES!!");
-                    loser.setVisibility(View.VISIBLE);
-                } else {
-                    lifeCount.setText(Integer.toString(count));
-                }
-            }
-        });
-
-        a5Button2 = (Button)findViewById(plus5Buttons[1]);
-        a5Button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView lifeCount = (TextView)findViewById(R.id.lives2);
-                int count = Integer.parseInt(lifeCount.getText().toString());
-                count = count + 5;
-                lifeCount.setText(Integer.toString(count));
-            }
-        });
-
-        // player3 buttons
-        mButton3 = (Button)findViewById(minusButtons[2]);
-        mButton3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView lifeCount = (TextView)findViewById(R.id.lives3);
-                int count = Integer.parseInt(lifeCount.getText().toString());
-                count = count - 1;
-                if (count <= 0) {
-                    lifeCount.setText("0");
-                    TextView loser = (TextView)findViewById(R.id.loser);
-                    loser.setText("PLAYER 3 LOSES!!");
-                    loser.setVisibility(View.VISIBLE);
-                } else {
-                    lifeCount.setText(Integer.toString(count));
-                }
-            }
-        });
-
-        aButton3 = (Button)findViewById(plusButtons[2]);
-        aButton3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView lifeCount = (TextView)findViewById(R.id.lives3);
-                int count = Integer.parseInt(lifeCount.getText().toString());
-                count = count + 1;
-                lifeCount.setText(Integer.toString(count));
-            }
-        });
-
-        m5Button3 = (Button)findViewById(minus5Buttons[2]);
-        m5Button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView lifeCount = (TextView)findViewById(R.id.lives3);
-                int count = Integer.parseInt(lifeCount.getText().toString());
-                count = count - 5;
-                if (count <= 0) {
-                    lifeCount.setText("0");
-                    TextView loser = (TextView)findViewById(R.id.loser);
-                    loser.setText("PLAYER 3 LOSES!!");
-                    loser.setVisibility(View.VISIBLE);
-                } else {
-                    lifeCount.setText(Integer.toString(count));
-                }
-            }
-        });
-
-        a5Button3 = (Button)findViewById(plus5Buttons[2]);
-        a5Button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView lifeCount = (TextView)findViewById(R.id.lives3);
-                int count = Integer.parseInt(lifeCount.getText().toString());
-                count = count + 5;
-                lifeCount.setText(Integer.toString(count));
-            }
-        });
-
-        // player4 buttons
-        mButton4 = (Button)findViewById(minusButtons[3]);
-        mButton4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView lifeCount = (TextView)findViewById(R.id.lives4);
-                int count = Integer.parseInt(lifeCount.getText().toString());
-                count = count - 1;
-                if (count <= 0) {
-                    lifeCount.setText("0");
-                    TextView loser = (TextView)findViewById(R.id.loser);
-                    loser.setText("PLAYER 4 LOSES!!");
-                    loser.setVisibility(View.VISIBLE);
-                } else {
-                    lifeCount.setText(Integer.toString(count));
-                }
-            }
-        });
-
-        aButton4 = (Button)findViewById(plusButtons[3]);
-        aButton4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView lifeCount = (TextView)findViewById(R.id.lives4);
-                int count = Integer.parseInt(lifeCount.getText().toString());
-                count = count + 1;
-                lifeCount.setText(Integer.toString(count));
-            }
-        });
-
-        m5Button4 = (Button)findViewById(minus5Buttons[3]);
-        m5Button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView lifeCount = (TextView)findViewById(R.id.lives4);
-                int count = Integer.parseInt(lifeCount.getText().toString());
-                count = count - 5;
-                if (count <= 0) {
-                    lifeCount.setText("0");
-                    TextView loser = (TextView)findViewById(R.id.loser);
-                    loser.setText("PLAYER 4 LOSES!!");
-                    loser.setVisibility(View.VISIBLE);
-                } else {
-                    lifeCount.setText(Integer.toString(count));
-                }
-            }
-        });
-
-        a5Button4 = (Button)findViewById(plus5Buttons[3]);
-        a5Button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView lifeCount = (TextView)findViewById(R.id.lives4);
-                int count = Integer.parseInt(lifeCount.getText().toString());
-                count = count + 5;
-                lifeCount.setText(Integer.toString(count));
-            }
-        });
     }
 }
